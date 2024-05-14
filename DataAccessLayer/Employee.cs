b83617a02108793ad54022a12d9806a62bfe2073
nandlal.prasad <nandlal.prasad@presto.co.in>
2024-05-14 13:02:04 +0530

    employeemodel

--- DataAccessLayer/Employee.cs
+++ DataAccessLayer/Employee.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +13,17 @@ namespace DataAccessLayer
 {
     public class Employee
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string City { get; set; }
+        [DisplayName("Coutry Name")]
+        public string countryName { get; set; }
+        [NotMapped]
+        public IFormFile image { get; set; }
+        
+        public string imageFileName { get; set; }
     }
 }
