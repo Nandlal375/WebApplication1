c2cdc68c8c8c471819e750d23e6c3bda3e8d3481
nandlal.prasad <nandlal.prasad@presto.co.in>
2024-05-30 17:00:29 +0530

    Programme.cs file update

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using WebApplication1.DataContext;
@@ -14,15 +13,15 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
-
             builder.Services.AddControllers();
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            builder.Services.AddScoped<IRegistrationRepository, RegistrationRepository>();
+            
             var app = builder.Build();
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -30,9 +29,12 @@ namespace WebApplication1
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            
             app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapControllers();
+            app.UseRouting();
+            
             app.Run();
         }
     }
